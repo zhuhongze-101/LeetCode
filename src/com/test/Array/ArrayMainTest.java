977051dc5c483c48cedbcb6de9167c32dac67216
zhuhongze-101 <928397941@qq.com>
2021-03-28 23:48:55 +0800

    双指针删除数组重复项

--- src/com/test/Array/ArrayMainTest.java
+++ src/com/test/Array/ArrayMainTest.java
@@ -6,7 +6,9 @@ public class ArrayMainTest {
     public static void main(String[] args) {
         ArraySolution solution = new ArraySolution();
         System.out.println(solution.longestCommonPrefix(new String[]{"dog","racecar","car"}));
-        System.out.println(solution.longestCommonPrefix(new String[]{"flower","flow","flight"}));
+
+        System.out.println(solution.removeDuplicates(new int[]{1,1,1,2}));
+
 
     }
 
--- src/com/test/Array/ArraySolution.java
+++ src/com/test/Array/ArraySolution.java
@@ -54,4 +54,28 @@ public class ArraySolution {
         return  result;
     }
 
+    /**
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+     * 双指针直接在相应的数据的修改，节省一个相应的数据空间 原地修改数据结构
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return 1;
+        int i = 0;
+        int j = 1;
+        while (j< nums.length){
+           if(nums[i] != nums[j]){
+               i++;
+               nums[i] = nums[j];
+           }
+           j++;
+        }
+
+
+        return i+1;
+    }
 }
